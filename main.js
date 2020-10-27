98a8670ee9a8e4ae6bc93f08d09fff946724bb3b
gabrielzy18 <youssef.gabriel18@gmail.com>
2020-10-27 09:31:28 -0300

    adicionado main.js e style.css e alteracoes em index.html

---
+++ main.js
@@ -0,0 +1,24 @@
+function adicionarTexto()
+{
+    var texto = document.getElementById('textbox').value
+    var nome = document.getElementById('nome').value
+    var mydiv = document.getElementById('mydiv')
+    var data = new Date();
+    const option = {
+        year: 'numeric',
+        month: 'long',
+        weekday: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+    }
+    data = data.toLocaleDateString('pt-br', option)
+    var conteudo = document.createElement('div')
+    conteudo.innerHTML = texto + "<br /> Escrito por: " + nome + "<br />"+ data
+    mydiv.appendChild(conteudo)
+    console.log(texto, nome)
+    
+}
+
+function changeNavigation(id) { document.getElementById('main').innerHTML = document.getElementById(id).innerHTML }
