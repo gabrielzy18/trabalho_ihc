214291076b37860b444063f32c7da17a04c3a8cb
Cr1ms00n <61632746+Cr1ms00n@users.noreply.github.com>
2020-11-16 22:09:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,14 +25,14 @@ function adicionarTexto() {
         }
         data = (data.toLocaleDateString('pt-br', option)).italics()
         var conteudo = document.createElement('div')
-        conteudo.innerHTML = "<b> Comentario</b>: " + texto + "<br /> <b>Escrito por</b>: " + nome + "<br />" +
+        conteudo.innerHTML = "<b> Comentário</b>: " + texto + "<br /> <b>Escrito por</b>: " + nome + "<br />" +
                              "<b>Avaliação do usuário</b>: " + aval + "<br />" + data + "<br /><br />"
         conteudo.style = "margin-left: 5%;"
         recebe_comentarios.appendChild(conteudo)
         global_aval_banha += parseInt(aval)
         global_aval_banha_cont++
         var media = global_aval_banha / global_aval_banha_cont
-        document.getElementById("banha").innerHTML = "<b>Avalição geral</b>: " + media.toFixed(1) +
+        document.getElementById("banha").innerHTML = "<b>Avaliação geral</b>: " + media.toFixed(1) +
                                                      "<br /><b>Total de avaliações</b>: " + global_aval_banha_cont
     }
 }
